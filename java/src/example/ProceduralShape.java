979cf777fe67727915a00e07f2c7d7ee76f84a51
Rachel Finucane <rachelkfinucane@gmail.com>
2020-03-20 15:22:39 +0000

    made 5 boxes rotate around each other

--- java/src/example/ProceduralShape.java
+++ java/src/example/ProceduralShape.java
@@ -39,6 +39,9 @@ public class ProceduralShape extends Visual
 
     float smoothedPyrSize = 0;
     float angle = 0;
+    int num = 10;
+    int polarity = 1;
+    float offset = 0;
 
     public void draw()
     {
@@ -49,39 +52,22 @@ public class ProceduralShape extends Visual
         stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
         // camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
         // translate(0, 0, -250);
+        translate(width/2, height/2);
                
         float boxSize = 50 + (getAmplitude() * 300);//map(average, 0, 1, 100, 400); 
         smoothedPyrSize = lerp(smoothedPyrSize, boxSize, 0.2f);  
 
-        // pushMatrix();
-        translate(width/2, height/2);
-        // popMatrix();
-
-        // rotateX(angle);
-        // rotateY(-angle);
-        rotateZ(angle);
-
         noFill();
-
-        beginShape();
-        vertex(-smoothedPyrSize, -smoothedPyrSize, -smoothedPyrSize);
-        vertex( smoothedPyrSize, -smoothedPyrSize, -smoothedPyrSize);
-        vertex(   0,    0,  smoothedPyrSize);
-
-        vertex( smoothedPyrSize, -smoothedPyrSize, -smoothedPyrSize);
-        vertex( smoothedPyrSize,  smoothedPyrSize, -smoothedPyrSize);
-        vertex(   0,    0,  smoothedPyrSize);
-
-        vertex( smoothedPyrSize, smoothedPyrSize, -smoothedPyrSize);
-        vertex(-smoothedPyrSize, smoothedPyrSize, -smoothedPyrSize);
-        vertex(   0,   0,  smoothedPyrSize);
-
-        vertex(-smoothedPyrSize,  smoothedPyrSize, -smoothedPyrSize);
-        vertex(-smoothedPyrSize, -smoothedPyrSize, -smoothedPyrSize);
-        vertex(   0,    0,  smoothedPyrSize);
-        endShape();
+        for(int i = 0; i < 5; i++)
+        {
+            rotateZ(i * (2 * PI / 5) + angle);
+            rotateY(i * (2 * PI / 5) + angle);
+            rotateX(i * (2 * PI / 5) - angle);
+            box(smoothedPyrSize);
+        }
 
         angle += map(getSmoothedAmplitude(), 0, 1, 0, 0.5f);
-        
+        polarity = polarity * - 1;
+        offset = random(1, height / 4);        
     }
 }
