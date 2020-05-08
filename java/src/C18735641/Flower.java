479e51dbbf59bdbeda9333c807e7c9668f894963
Rachel Finucane <rachelkfinucane@gmail.com>
2020-05-08 13:21:34 +0100

    rotated each row of petals - looks like a lilygit add .!

--- java/src/C18735641/Flower.java
+++ java/src/C18735641/Flower.java
@@ -12,6 +12,8 @@ public class Flower {
     public void render() {
         int numPetals = 8;
         int numRows = 5;
+        float pOffset = PConstants.PI * 0.3f;
+        float rowAngleOffset = 0.1f;
 
         // size of petal
         float pX = (270f / numPetals);
@@ -24,6 +26,7 @@ public class Flower {
         float rColor = 0;
 
         mv.pushMatrix(); // push default screen
+
         // translate to center of screen
         mv.translate(mv.width / 2, mv.height / 2);
         // rotate rose to see side on
@@ -35,17 +38,25 @@ public class Flower {
             mv.pushMatrix(); // push translated screen
             mv.fill(rColor, 255, 255);
             for (int j = 0; j < numPetals; j++) {
+                mv.pushMatrix();
+                mv.rotateX(PConstants.PI * rowAngleOffset);
                 mv.ellipse(rX, rY, pX, pY);
+                
+                mv.popMatrix();
                 mv.rotate(PConstants.TWO_PI / numPetals);
             }
 
             // pop translated screen (remove petal rotation)
             mv.popMatrix();
+            
+            // offset petals for next row
+            mv.rotate(pOffset);
 
             // reduce petal size
-            pX = pX * 0.8f;
-            pY = pY * 0.8f;
+            pY = pY * 0.9f;
             rColor += (255 / numRows);
+
+            rowAngleOffset += 0.1f;
         }
 
         mv.pushMatrix(); // push translated screen
