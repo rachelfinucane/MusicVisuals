14f7b733a594cee1cf4b397aa792f86216b3cbdb
Rachel Finucane <rachelkfinucane@gmail.com>
2020-05-08 14:47:01 +0100

    setup new class Cluster

---
+++ java/src/C18735641/ClusterVisual.java
@@ -0,0 +1,17 @@
+package C18735641;
+
+import processing.core.*;
+
+public class ClusterVisual {
+    MyVisual mv;
+
+    public ClusterVisual(MyVisual mv)
+    {
+        this.mv = mv; 
+    }
+
+    public void render()
+    {
+        
+    }
+}
