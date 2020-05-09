1d4f02d77d5ea490949bc843b0be47136f681270
Rachel Finucane <rachelkfinucane@gmail.com>
2020-05-10 00:51:33 +0100

    fixed tree, changes with amplitude and freq bands, leaves also

--- java/src/C18735641/BranchVisual.java
+++ java/src/C18735641/BranchVisual.java
@@ -13,40 +13,29 @@ public class BranchVisual {
         this.mv = mv;
     }
 
-    /*
+    public void render(float startX, float startY, float height) {
 
-    public void render(float startX, float startY, float height, float angle, int gen) {
-        float x, z;
-        
         mv.stroke(255);
         mv.fill(255);
-        // trunk first
-        mv.pushMatrix();
-        //mv.translate(mv.width / 2, mv.height / 2);
-        // translate and rotate to where we're told
         // mv.translate(startX, startY, 0);
-        // float nextX = startX * MyVisual.cos(gen * angle) - startX * MyVisual.sin(gen * angle);
-        // float nextY = startY * MyVisual.cos(gen * angle) +  startY * MyVisual.sin(gen * angle);
-        // mv.rotateZ(angle);
-
-        // mv.line(startX, startY, 0, nextX, nextY, 0);
-        mv.box(10);
         // for (int i = 0; i < numLines; i++) {
-        //     angle = MyVisual.map(i, 0, numLines, 0, MyVisual.TWO_PI);
+        // angle = MyVisual.map(i, 0, numLines, 0, MyVisual.TWO_PI);
 
-        //     x = MyVisual.cos(angle) * distance;
-        //     z = MyVisual.sin(angle) * distance;
+        // x = MyVisual.cos(angle) * distance;
+        // z = MyVisual.sin(angle) * distance;
 
-        //     mv.line(x, 0, z, x, height, z);
+        // mv.line(x, 0, z, x, -height, z);
         // }
 
-        mv.popMatrix();
+        // float rowAngleOffset = MyVisual.map(i, 0, numLeafs, MyVisual.HALF_PI, 0);
+        // // mv.pushMatrix();
+        // mv.rotateX(rowAngleOffset);
+        // mv.ellipse(rX, rY + (pY / 2), pX, pY);
 
-    }
-    */
+        // mv.popMatrix();
+        // mv.rotate(MyVisual.TWO_PI / numPetals);
 
-    public void render()
-    {
-        
+        mv.line(0, 0, 0, -height);
     }
+
 }
--- java/src/C18735641/TreeVisual.java
+++ java/src/C18735641/TreeVisual.java
@@ -6,64 +6,114 @@ public class TreeVisual {
     MyVisual mv;
     int maxGenerations = 2;
     int numBranches = 2;
-    float branchHeight = 70;
-    float angle = MyVisual.THIRD_PI;
-
+    float branchHeight = 300;
+    float staticAngle = MyVisual.QUARTER_PI;
+    float angle;
+    float branchReduction = 0.6f;
     float margin = 50;
+    float leafMin = branchHeight * 0.2f;
+    float leafMax = branchHeight * 0.4f;
+    float trunkMin = 20f;
+    float trunkMax = 40f;
 
     public TreeVisual(MyVisual mv) {
         this.mv = mv;
     }
 
     public void render() {
+        mv.stroke(255);
+        mv.pushMatrix();
+        mv.translate(mv.width / 2, mv.height - margin);
+        drawBranch(branchHeight);
+        mv.popMatrix();
+        BranchVisual b = new BranchVisual(mv);
+        b.render(mv.width / 2, mv.height - margin, branchHeight);
+        mv.translate(mv.width / 2, mv.height - margin - branchHeight);
+        drawSelf(branchHeight);
+        // drawLeaves(branchHeight);
 
-        // trunk first
-        // drawSelf(mv.width / 2f, mv.height / 10f, 0, branchHeight, 0);
-        float startX = mv.width / 2;
-        float startY = mv.height - margin;
+        angle = MyVisual.map(mv.getSmoothedAmplitude(), 0, 1, 1f * staticAngle, 1.5f * staticAngle);
+    }
 
-        for (int i = 0; i < maxGenerations; i++) {
-            mv.stroke(255);
-            // float endX = startX;
-            // float endY = mv.height - 200;
-
-            float endX = startX * MyVisual.cos(i * angle) + startY * MyVisual.sin(i * angle);
-            float endY = -startY * MyVisual.cos(i * angle) + startX * MyVisual.sin(i * angle);
+    private void drawSelf(float bHeight) {
+        bHeight = bHeight * branchReduction;
+        if (bHeight > 30) {
             mv.pushMatrix();
-            mv.translate(endX, endY);
-            mv.box(50);
+            mv.rotate(angle);
+            drawBranch(bHeight);
+            mv.translate(0, -bHeight);
+            drawSelf(bHeight);
             mv.popMatrix();
 
-            mv.line(startX, startY, endX, endY);
-            startX = endX;
-            startY = endY;
+            mv.pushMatrix();
+            mv.rotate(-angle);
+            drawBranch(bHeight);
+            mv.translate(0, -bHeight);
+            drawSelf(bHeight);
+            mv.popMatrix();
 
         }
 
-        // x * MyVisual.cos(gen * angle) - y * MyVisual.sin(gen * angle);
-        // nextY = y * MyVisual.cos(gen * angle) + x * MyVisual.sin(gen * angle);
-
     }
 
-    /*
-     * 
-     * private void drawSelf(float x, float y, float a, float height, int gen) {
-     * float nextX, nextY; // nextX = (1 / (gen + 1) ) * MyVisual.sin(angle) + x; //
-     * so far, this will have to change in the for loop // nextY = -(1 / (gen + 1))
-     * * MyVisual.cos(angle) + y; nextX = x * MyVisual.cos(gen * angle) - y *
-     * MyVisual.sin(gen * angle); nextY = y * MyVisual.cos(gen * angle) + x *
-     * MyVisual.sin(gen * angle);
-     * 
-     * // if (gen < maxGenerations) { // // for(int i = 0; i < numBranches; i++) //
-     * // { // // BranchVisual br = new BranchVisual(mv); // // br.render(0f, 0f,
-     * 0f); // // drawSelf(gen); // // } // BranchVisual br = new BranchVisual(mv);
-     * // br.render(x, y, branchHeight, a * gen, gen); // drawSelf(nextX, nextY, a,
-     * height, gen + 1); // }
-     * 
-     * for(int i = 0; i < 3; i++) { BranchVisual br = new BranchVisual(mv);
-     * br.render(x, y, branchHeight, a * i); }
-     * 
-     * }
-     * 
-     */
+    private void drawBranch(float bHeight) {
+        int numLines = mv.getBands().length;
+        float distance = MyVisual.map(mv.getSmoothedAmplitude(), 0, 1, trunkMin, trunkMax);
+        float lnAngle;
+        // float leafHeight;
+        // float leafX;
+
+        float x, z;
+        for (int i = 0; i < numLines; i++) {
+            lnAngle = MyVisual.map(i, 0, numLines, 0, MyVisual.TWO_PI);
+
+            x = MyVisual.cos(lnAngle) * distance;
+            z = MyVisual.sin(lnAngle) * distance;
+
+            // leafX = MyVisual.cos(lnAngle) * 1.3f * distance;
+            // leafHeight = MyVisual.map(mv.getSmoothedBands()[i], 0, 1000, leafMin, leafMax);
+
+            mv.line(x, 0, z, x, -bHeight, z);
+            // mv.fill(200, 255, 255);
+            // mv.noStroke();
+            // mv.ellipse(leafX, -bHeight - (leafHeight / 2), distance * 0.8f, leafHeight);
+            // mv.stroke(255);
+        }
+        if(bHeight < branchHeight / 2)
+        {
+            drawLeaves(bHeight);
+        }
+    }
+
+    private void drawLeaves(float bHeight)
+    {
+        int numLines = mv.getBands().length;
+        float distance = MyVisual.map(mv.getSmoothedAmplitude(), 0, 1, trunkMin, trunkMax);
+        float lnAngle;
+        float leafHeight;
+        float leafX;
+        float colour = 0;
+
+        for (int i = 0; i < numLines; i++) {
+            lnAngle = MyVisual.map(i, 0, numLines, 0, MyVisual.TWO_PI);
+
+            // lnAngle = MyVisual.map(i, 0, numLines, 0, MyVisual.TWO_PI);
+            colour = MyVisual.map(i, 0, numLines, 0, 255);
+
+            leafX = MyVisual.cos(lnAngle) * 1.3f * distance;
+            leafHeight = MyVisual.map(mv.getSmoothedBands()[i], 0, 1000, leafMin, leafMax);
+
+            mv.fill(colour, 255, 255);
+            mv.noStroke();
+            mv.pushMatrix();
+            mv.rotate(lnAngle);
+            mv.ellipse(leafX, -bHeight - (leafHeight / 2), 1.5f * distance, leafHeight);
+            // mv.ellipse(0, distance / 2, distance * 0.8f, leafHeight);
+            mv.popMatrix();
+            // mv.rotate(MyVisual.TWO_PI / numLines);
+            
+        }
+        mv.stroke(255);
+    }
+   
 }
