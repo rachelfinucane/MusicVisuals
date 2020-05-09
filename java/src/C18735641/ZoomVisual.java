ab6fda8f6dee0d3d11a347ae927fd1edc21ac18b
Rachel Finucane <rachelkfinucane@gmail.com>
2020-05-09 12:38:27 +0100

    started new feature - zooming circles

---
+++ java/src/C18735641/ZoomVisual.java
@@ -0,0 +1,23 @@
+package C18735641;
+import processing.core.*;
+
+public class ZoomVisual {
+    MyVisual mv;
+
+    public ZoomVisual(MyVisual mv)
+    {
+        this.mv = mv; 
+    }
+
+    public void render()
+    {
+        float gap = mv.width / (float) mv.getBands().length;
+        mv.noStroke();
+        for(int i = 0 ; i < mv.getBands().length ; i ++)
+        {
+            mv.fill(PApplet.map(i, 0, mv.getBands().length, 255, 0), 255, 255);
+            mv.rect(i * gap, mv.height, gap,-mv.getSmoothedBands()[i] * 0.2f); 
+        }
+    }
+    
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,8 +1,6 @@
 package ie.tudublin;
 
-import example.CubeVisual;
-import example.MyVisual;
-import example.RotatingAudioBands;
+import C18735641.MyVisual;
 
 public class Main
 {	
@@ -10,7 +8,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
+        processing.core.PApplet.runSketch( a, new MyVisual());		
 	}
 
 	public static void main(String[] args)
